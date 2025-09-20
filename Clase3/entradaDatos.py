adcb51f8a430f30358f403bd7d2f9984c44ab9ec
Sharo Amorin <msharo0803@gmail.com>
2025-09-20 19:32:52 -0300

    clase 2 y 3

---
+++ Clase3/entradaDatos.py
@@ -0,0 +1,11 @@
+'''
+entrada de datos en python unida
+'''
+
+cadena=input("Como se llama tu proyecto:")
+
+
+print(f"Tu proyecto se llama: {cadena}")
+
+cadena=input("que version es: ")
+print(f"Tu version es: {cadena+1}") #Error
