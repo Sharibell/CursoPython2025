8ad49eb7339ae236ec3fcd095ec90f1611378dad
Sharo Amorin <msharo0803@gmail.com>
2025-09-20 22:25:28 -0300

    clase 3

--- Clase3/entradaDatos.py
+++ Clase3/entradaDatos.py
@@ -1,11 +1,20 @@
 '''
-entrada de datos en python unida
+Entrada de datos en python unida
+
+cadena2= input("Cual es tu proyecto:")
+
+#Forma moderna
+print(f"Tu proyecto se llama: {cadena} y es de: {cadena2}")
+
+#Forma antigua
+print("Tu proyecto se llama: ",cadena," y es de: ",cadena2)
 '''
 
 cadena=input("Como se llama tu proyecto:")
+print(f"Tu proyecto es: {cadena}")
 
+#cadena=input("que version es: ")
+#print(f"Tu version es: {cadena+1}") #error
 
-print(f"Tu proyecto se llama: {cadena}")
-
-cadena=input("que version es: ")
-print(f"Tu version es: {cadena+1}") #Error
+cadena=int(input("que version es: "))
+print(f"Tu version es: {cadena+1}") #ok
