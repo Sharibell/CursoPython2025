5c94618a4d8fd370ecffec9ae10c5e4d5014e232
Sharo Amorin <msharo0803@gmail.com>
2025-10-11 01:44:04 -0300

    clase6

--- Clase6/login.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-login= Blueprint('login', __name__)
-
-@login.route('/login', methods=['POST'])
-def login_user():
-    print ("Headers: ", request.headers)
